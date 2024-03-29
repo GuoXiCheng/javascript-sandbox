aafcdc403ab7294e4252a45c5c820b7b04bcfcbd
Xicheng Guo <xicheng_guo@wistron.com>
2024-03-29 15:46:36 +0800

    update

---
+++ src/date.js
@@ -0,0 +1,8 @@
+const date = new Date();
+console.log(`date: ${date}`); // Fri Mar 29 2024 15:45:48 GMT+0800 (中国标准时间)
+
+console.log(`getDate: ${date.getDate()}`); // 29
+
+console.log(`getDay: ${date.getDay()}`); // 5
+
+console.log(`getMonth: ${date.getMonth()}`); // 2
