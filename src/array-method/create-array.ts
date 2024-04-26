8203e3a8403061e32bccdc78756d624b3ea55fa8
Xicheng Guo <xicheng_guo@wistron.com>
2024-04-26 09:48:04 +0800

    update array method

--- src/array-method/create-array.ts
+++ src/array-method/create-array.ts
@@ -1,37 +1,42 @@
 // run with: ts-node src/array-method/create-array.ts
+export {};
+
+let arr;
 
 // #region literal-quantity
-const arr1 = [1, 2, 3, 4, 5]; // 创建包含指定元素的数组
+arr = [1, 2, 3, 4, 5]; // 创建包含指定元素的数组
 
-const arr2 = []; // 创建一个空数组
+arr = []; // 创建一个空数组
 // #endregion literal-quantity
 
 // #region extension-operator
-const arr3 = [1, ...[2, 3, 4], 5]; // [ 1, 2, 3, 4, 5 ]
+arr = [1, ...[2, 3, 4], 5]; // [ 1, 2, 3, 4, 5 ]
 
-const arr4 = [..."Array"]; // [ 'A', 'r', 'r', 'a', 'y' ]
+arr = [..."Array"]; // [ 'A', 'r', 'r', 'a', 'y' ]
 // #endregion extension-operator
 
 // #region constructor
-const arr5 = new Array(1, 2, 3, 4, 5); // 创建包含指定元素的数组
+arr = new Array(1, 2, 3, 4, 5); // 创建包含指定元素的数组
 
-const arr6 = new Array(5); // 创建一个长度为5的数组，元素都是undefined
+arr = new Array(5); // 创建一个长度为5的数组，元素都是undefined
 // #endregion constructor
 
 // #region array-from-1
-const arr7 = Array.from("Array"); // [ 'A', 'r', 'r', 'a', 'y' ]
+arr = Array.from("Array"); // [ 'A', 'r', 'r', 'a', 'y' ]
 // #endregion array-from-1
 
 // #region array-from-2
-const arr8 = Array.from(new Map().set(1, 2).set(3, 4)); // [ [ 1, 2 ], [ 3, 4 ] ]
+arr = Array.from(new Map().set(1, 2).set(3, 4)); // [ [ 1, 2 ], [ 3, 4 ] ]
 
-const arr9 = Array.from(new Set([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
+arr = Array.from(new Set([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
 // #endregion array-from-2
 
 // #region array-from-3
-const arr10 = Array.from([1, 2, 3, 4, 5], (x) => x * 2);
+arr = Array.from([1, 2, 3, 4, 5], (x) => x * 2);
 // #endregion array-from-3
 
 // #region array-of
-const arr11 = Array.of(1, 2, 3, 4, 5); // [ 1, 2, 3, 4, 5 ]
+arr = Array.of(1, 2, 3, 4, 5); // [ 1, 2, 3, 4, 5 ]
 // #endregion array-of
+
+console.log(arr);
---
+++ src/array-method/iterative-array.ts
@@ -0,0 +1,70 @@
+// run with: ts-node src/array-method/iterative-array.ts
+
+export {};
+
+const arr = [1, 2, 3, 4, 5];
+
+console.log("正向循环");
+// #region for-loop-1
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+// #endregion for-loop-1
+
+console.log("反向循环");
+// #region for-loop-2
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(arr[i]);
+}
+// #endregion for-loop-2
+
+console.log("跳过循环");
+// #region for-loop-3
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) continue;
+  console.log(arr[i]);
+}
+// #endregion for-loop-3
+
+console.log("循环中断");
+// #region for-loop-4
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) break;
+  console.log(arr[i]);
+}
+
+console.log("for-of循环");
+// #region for-of-1
+for (const item of arr) {
+  console.log(item);
+}
+// #endregion for-of-1
+
+console.log("for-of循环（索引）");
+// #region for-of-2
+for (const [index, item] of arr.entries()) {
+  console.log(index, item);
+}
+// #endregion for-of-2
+
+console.log("forEach循环");
+// #region forEach-1
+arr.forEach((item, index, array) => {
+  console.log(item, index, array);
+});
+// #endregion forEach-1
+
+console.log("forEach循环中断");
+// #region forEach-2
+try {
+  arr.forEach((item) => {
+    if (item % 2 === 0) throw new Error("Loop interrupted");
+    console.log(item);
+  });
+} catch (e: any) {
+  if (e.message !== "Loop interrupted") {
+    // 如果异常不是预期中的中断循环异常，重新抛出
+    throw e;
+  }
+}
+// #endregion forEach-2
