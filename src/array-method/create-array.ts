a0e31ebf23d9a5e8148ebb8388d28d051ce1d202
Xicheng Guo <xicheng_guo@wistron.com>
2024-04-26 10:03:07 +0800

    update array method

--- src/array-method/create-array.ts
+++ src/array-method/create-array.ts
@@ -1,42 +1,40 @@
 // run with: ts-node src/array-method/create-array.ts
 export {};
 
-let arr;
-
 // #region literal-quantity
-arr = [1, 2, 3, 4, 5]; // 创建包含指定元素的数组
+const resOfEle = [1, 2, 3, 4, 5]; // 创建包含指定元素的数组
 
-arr = []; // 创建一个空数组
+const resOfEmpty = []; // 创建一个空数组
 // #endregion literal-quantity
 
 // #region extension-operator
-arr = [1, ...[2, 3, 4], 5]; // [ 1, 2, 3, 4, 5 ]
+const resOfExt = [1, ...[2, 3, 4], 5]; // [ 1, 2, 3, 4, 5 ]
 
-arr = [..."Array"]; // [ 'A', 'r', 'r', 'a', 'y' ]
+const resOfExtStr = [..."Array"]; // [ 'A', 'r', 'r', 'a', 'y' ]
 // #endregion extension-operator
 
 // #region constructor
-arr = new Array(1, 2, 3, 4, 5); // 创建包含指定元素的数组
+const resOfConEle = new Array(1, 2, 3, 4, 5); // 创建包含指定元素的数组
 
-arr = new Array(5); // 创建一个长度为5的数组，元素都是undefined
+const resOfConEmpty = new Array(5); // 创建一个长度为5的数组，元素都是undefined
 // #endregion constructor
 
 // #region array-from-1
-arr = Array.from("Array"); // [ 'A', 'r', 'r', 'a', 'y' ]
+const resOfArrFrom = Array.from("Array"); // [ 'A', 'r', 'r', 'a', 'y' ]
 // #endregion array-from-1
 
 // #region array-from-2
-arr = Array.from(new Map().set(1, 2).set(3, 4)); // [ [ 1, 2 ], [ 3, 4 ] ]
+const resOfArrMap = Array.from(new Map().set(1, 2).set(3, 4)); // [ [ 1, 2 ], [ 3, 4 ] ]
 
-arr = Array.from(new Set([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
+const resOfArrSet = Array.from(new Set([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
 // #endregion array-from-2
 
 // #region array-from-3
-arr = Array.from([1, 2, 3, 4, 5], (x) => x * 2);
+const resOfArrFromFun = Array.from([1, 2, 3, 4, 5], (x) => x * 2); // [ 2, 4, 6, 8, 10 ]
 // #endregion array-from-3
 
 // #region array-of
-arr = Array.of(1, 2, 3, 4, 5); // [ 1, 2, 3, 4, 5 ]
+const resOfArrOf = Array.of(1, 2, 3, 4, 5); // [ 1, 2, 3, 4, 5 ]
 // #endregion array-of
 
-console.log(arr);
+console.log(resOfArrFromFun);
