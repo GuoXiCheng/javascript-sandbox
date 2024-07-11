3f79961e03dfa1d8639059378486c85c9796a068
Xicheng Guo <xicheng_guo@wistron.com>
2024-07-11 11:41:22 +0800

    update

--- src/module/es6-import.ts
+++ src/module/es6-import.ts
@@ -4,25 +4,25 @@
 import { name, sayHello } from "./es6-export";
 console.log(name); // module
 sayHello(); // Hello
-// #region import-named
+// #endregion import-named
 
 // #region import-default
 import moduleDefault from "./es6-export";
 moduleDefault(); // default module
-// #region import-default
+// #endregion import-default
 
 // #region import-bulk
 import * as Foo from "./es6-export";
 console.log(Foo.foo, Foo.bar, Foo.baz); // foo bar baz
-// #region import-bulk
+// #endregion import-bulk
 
 // #region rename-import
 import { ModuleName as moduleName } from "./es6-export";
 console.log(moduleName); // new module
-// #region rename-import
+// #endregion rename-import
 
 // #region dynamic-import
 import("./es6-export").then((module) => {
   console.log(module.dynamic); // dynamic
 });
-// #region dynamic-import
+// #endregion dynamic-import
