41ac83dd6266a29169dd505584052ce6d20255c5
Xicheng Guo <xicheng_guo@wistron.com>
2024-07-29 15:42:45 +0800

    update file-system

---
+++ src/file-system/stream-write.ts
@@ -0,0 +1,18 @@
+// run with: ts-node src/file-system/stream-write.ts
+import fs from "fs";
+
+const writeStream = fs.createWriteStream("example/file.txt");
+
+writeStream.on("open", () => {
+  writeStream.write("Hello World!\n");
+  writeStream.write("Another line!\n");
+  writeStream.end();
+});
+
+writeStream.on("finish", () => {
+  console.log("Write completed");
+});
+
+writeStream.on("error", (error) => {
+  console.error(error);
+});
